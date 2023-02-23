d38fc2b3531cf138075c38e6da021d048ba5e100
ariesdimasy <ariesdimasy@gmail.com>
2023-02-23 21:17:15 +0700

    setting env

---
+++ config/config.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+
+module.exports = {
+  development: {
+    username: process.env.DB_DEV_USER,
+    password: process.env.DB_DEV_PASSWORD,
+    database: process.env.DB_DEV_DATABASE,
+    host: process.env.DB_DEV_HOST,
+    port: process.env.DB_DEV_PORT,
+    dialect: process.env.DB_DEV_DIALECT,
+    dialectOptions: {
+      bigNumberStrings: true,
+    },
+  },
+  test: {
+    username: process.env.CI_DB_USERNAME,
+    password: process.env.CI_DB_PASSWORD,
+    database: process.env.CI_DB_NAME,
+    host: "127.0.0.1",
+    port: 3306,
+    dialect: "mysql",
+    dialectOptions: {
+      bigNumberStrings: true,
+    },
+  },
+  production: {
+    username: process.env.PROD_DB_USERNAME,
+    password: process.env.PROD_DB_PASSWORD,
+    database: process.env.PROD_DB_NAME,
+    host: process.env.PROD_DB_HOSTNAME,
+    port: process.env.PROD_DB_PORT,
+    dialect: "mysql",
+    dialectOptions: {
+      bigNumberStrings: true,
+      ssl: {
+        ca: fs.readFileSync(__dirname + "/mysql-ca-main.crt"),
+      },
+    },
+  },
+};
--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const dotenv = require("dotenv");
 const routes = require("./routers");
+
+dotenv.config();
 
 app.use(cors());
 app.use(express.json());
